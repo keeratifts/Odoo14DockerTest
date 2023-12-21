38d7b8dc0cb9e7f5e63ee4d980ae09dfa5cfa7fd
Keeratifts <k.fungtammasan@gmail.com>
2023-12-21 19:38:15 +0700

    first

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Real estate',
+    'version': '13.0.0',
+    'summary': 'Test Module',
+    'author': 'My Company Ltd',
+    'website': 'https://www.mycompany.com',
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
